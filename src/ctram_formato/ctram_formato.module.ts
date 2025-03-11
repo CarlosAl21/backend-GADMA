6f916ca059eb69a3d991ff796730674af87a2448
CarlosAl21 <g4to101@hotmail.com>
2025-03-11 08:18:17 -0500

    cosillas

--- src/ctram_formato/ctram_formato.module.ts
+++ src/ctram_formato/ctram_formato.module.ts
@@ -3,9 +3,10 @@ import { CtramFormatoService } from './ctram_formato.service';
 import { CtramFormatoController } from './ctram_formato.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CtramFormato } from './entities/ctram_formato.entity';
+import { CtramRequisito } from 'src/ctram_requisito/entities/ctram_requisito.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CtramFormato])],
+  imports: [TypeOrmModule.forFeature([CtramFormato, CtramRequisito])],
   controllers: [CtramFormatoController],
   providers: [CtramFormatoService],
   exports: [CtramFormatoService],
