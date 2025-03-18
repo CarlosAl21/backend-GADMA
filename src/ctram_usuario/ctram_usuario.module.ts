c8f0e0b8cd0d71c9d5102e546665cc4b4ae9b960
CarlosAl21 <g4to101@hotmail.com>
2025-03-18 15:43:32 -0500

    cambios menores

--- src/ctram_usuario/ctram_usuario.module.ts
+++ src/ctram_usuario/ctram_usuario.module.ts
@@ -3,9 +3,10 @@ import { CtramUsuarioService } from './ctram_usuario.service';
 import { CtramUsuarioController } from './ctram_usuario.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CtramUsuario } from './entities/ctram_usuario.entity';
+import { CtramDireccion } from 'src/ctram_direccion/entities/ctram_direccion.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CtramUsuario])],
+  imports: [TypeOrmModule.forFeature([CtramUsuario, CtramDireccion])],
   controllers: [CtramUsuarioController],
   providers: [CtramUsuarioService],
   exports: [CtramUsuarioService],
